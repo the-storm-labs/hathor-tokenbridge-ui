edfa5f1d70a0263b544113ca77fcd67c8f12335f
Yan Martins <yan.martins@gmail.com>
2025-10-27 00:12:24 -0300

    fix: correct checksum logic for hathor address validation

--- src/js/index.js
+++ src/js/index.js
@@ -160,7 +160,7 @@ $(document).ready(function () {
   });
 
   $("#hathorAddress").keyup(function (event) {
-    validateHathorAddress();
+    handleHathorAddressChange();
   });
 
   $("#changeNetwork").on("click", function () {
@@ -173,11 +173,10 @@ $(document).ready(function () {
   isInstalled();
 });
 
-function validateHathorAddress() {
+function handleHathorAddressChange() {
   const hathorAddress = $("#hathorAddress").val();
-  console.log('111111');
   if (hathorAddress) {
-    if (HathorWallet.validateAddress(hathorAddress)) {
+    if (validateHathorAddress(hathorAddress)) {
       $("#hathorAddress").removeClass("is-invalid");
       $("#hathorAddress").addClass("is-valid");
     } else {
@@ -674,8 +673,7 @@ async function crossToken() {
     return;
   }
 
-  console.log('22222');
-  if (!HathorWallet.validateAddress(hathorAddress)) {
+  if (!validateHathorAddress(hathorAddress)) {
     crossTokenError("Invalid Hathor address!");
     return;
   }
