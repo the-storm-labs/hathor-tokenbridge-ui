f62c854da7a26525c4c2fcb591af48578ad296cb
Yan Martins <yan.martins@gmail.com>
2025-10-26 23:36:45 -0300

    fix: resolve ReferenceError by replacing bs58check with bitcoinjs-lib

--- src/js/validate-address.js
+++ src/js/validate-address.js
@@ -1,19 +1,9 @@
 const HathorWallet = {
   validateAddress(address) {
     try {
-      const addressBytes = bs58check.decode(address);
-
-      // Validate address length
-      if (addressBytes.length !== 25) {
-        console.error(`Invalid address: ${address}. Address has ${addressBytes.length} bytes and should have 25.`);
-        return false;
-      }
-
-      // Checksum is already validated by bs58check.decode()
-
+      bitcoinjs.address.fromBase58Check(address);
       return true;
     } catch (e) {
-      console.error(`Invalid address: ${address}.`, e);
       return false;
     }
   }
